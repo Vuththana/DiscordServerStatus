aeba5bb5b7d37fb093ebd4d9e4fd2c701a45fd07
Vuththana <135146361+Vuththana@users.noreply.github.com>
2025-08-23 19:45:23 +0700

    added embed/ip.yml loader

--- src/main/java/org/goros/discordServerStatus/DiscordServerStatus.java
+++ src/main/java/org/goros/discordServerStatus/DiscordServerStatus.java
@@ -1,21 +1,26 @@
 package org.goros.discordServerStatus;
 
 import net.dv8tion.jda.api.JDA;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.goros.discordServerStatus.discord.BotRegistry.BotRegistry;
 import org.goros.discordServerStatus.listener.DiscordListener;
+
+import java.io.File;
 
 public final class DiscordServerStatus extends JavaPlugin {
     private JDA jda;
     private String botToken;
     private String channelId;
     private String onlineStatus;
+    private File ipFile;
+    private FileConfiguration ipConfig;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-
-
+        createIpConfig();
         botToken = getConfig().getString("discord.bot-token");
         channelId = getConfig().getString("discord.channel-id");
         String prefix = getConfig().getString("discord.prefix", "!");
@@ -37,10 +42,22 @@ public final class DiscordServerStatus extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        if(jda != null) {
-            jda.shutdown();
-            getLogger().info("Discord bot disconnected");
+        if (jda != null) {
+            jda.shutdownNow(); // ✅ ensures all OkHttp threads stop
+            jda = null;
+            getLogger().info("Discord bot disconnected.");
         }
+    }
+    private void createIpConfig() {
+        ipFile = new File(getDataFolder(), "Embed/IP.yml");
+        if (!ipFile.exists()) {
+            saveResource("embed/ip.yml", false); // copy from JAR
+        }
+        ipConfig = YamlConfiguration.loadConfiguration(ipFile);
+    }
+
+    public FileConfiguration getIpConfig() {
+        return ipConfig;
     }
 
     public JDA getJda() {return jda;};
